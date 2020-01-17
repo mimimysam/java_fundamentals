0a9798001ce426aaa469012fdf5915260e7cca77
mimimysam <59676205+mimimysam@users.noreply.github.com>
2020-01-16 18:01:37 -0800

    completed exercise_01

--- src/labs_examples/datatypes_operators/labs/Exercise_01.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_01.java
@@ -17,6 +17,16 @@ public class Exercise_01 {
     public static void main(String[] args) {
 
         // write code here
+        int x = 12;
+        double y = 4.87;
+        float z = 32384;
+        char letter = 'H';
+
+        System.out.println("Int value " +x);
+        System.out.println("Double value " +y);
+        System.out.println("Float value " +z);
+        System.out.println("Char value " +letter);
+
 
     }
 }
