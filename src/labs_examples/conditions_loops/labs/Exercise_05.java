1b43e01bb4eb14a9a8e1d4b7ffcd58f041ba0195
mimimysam <59676205+mimimysam@users.noreply.github.com>
2020-01-20 20:40:53 -0800

    completed sum/average exercise_05

--- src/labs_examples/conditions_loops/labs/Exercise_05.java
+++ src/labs_examples/conditions_loops/labs/Exercise_05.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 5: Calculator
  *
@@ -16,4 +18,24 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_05 {
+    public static void main(String[] args) {
+
+        // create scanner
+        Scanner scanner = new Scanner(System.in);
+        // prompt user
+        System.out.print("Enter first number: ");
+        // assign input to variable as int
+        int large = scanner.nextInt();
+        System.out.print("Enter a number smaller than the first number: ");
+        int small = scanner.nextInt();
+        int sum = 0;
+        for (int i = small; i <= large; i++) {
+                sum = sum + i;
+        }
+        float bottom = large - small + 1;
+        float average = sum / bottom;
+
+        System.out.println("The sum is: " + sum);
+        System.out.println("The average is: " + average);
+    }
 }
