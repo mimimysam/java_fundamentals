1ec81034df3d4dd72d9369cbfed1007146877c7a
mimimysam <59676205+mimimysam@users.noreply.github.com>
2020-01-20 21:42:30 -0800

    completed nested loop array exercise_11

--- src/labs_examples/conditions_loops/labs/Exercise_11.java
+++ src/labs_examples/conditions_loops/labs/Exercise_11.java
@@ -22,5 +22,14 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_11 {
-
+    public static void main(String[] args) {
+        int[][] table = new int[11][11];
+        for (int i=1; i<table.length; i++){
+            for (int j=1; j<table[i].length; j++){
+                table[i][j] = i*j;
+                System.out.print(table[i][j] + "  |  ");
+            }
+            System.out.println();
+        }
+    }
 }
