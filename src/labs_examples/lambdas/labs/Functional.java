56ed5297b01d83735175704dad86867faefd993b
mimimysam <59676205+mimimysam@users.noreply.github.com>
2020-03-05 08:42:51 +0800

    lambda exercises

--- src/labs_examples/lambdas/labs/Functional.java
+++ src/labs_examples/lambdas/labs/Functional.java
@@ -20,3 +20,48 @@ package labs_examples.lambdas.labs;
  *      7) Demonstrate the use of at least two built-in functional interfaces from the java.util.function package.
  *
  */
+
+public class Functional {
+
+    @FunctionalInterface
+    public interface MyFunctionalInterface {
+        void method();
+    }
+
+    @FunctionalInterface
+    public interface AnotherFunctInter {
+        double half(double a);
+    }
+
+    @FunctionalInterface
+    public interface TwoParameters {
+        int difference(int x, int y);
+    }
+
+}
+
+class Interface {
+
+    public static void main(String[] args) {
+        Functional.MyFunctionalInterface obj = new Functional.MyFunctionalInterface() {
+            @Override
+            public void method() {
+                System.out.println("This is printing from the Anonymous Inner Class");
+            }
+        };
+        obj.method();
+
+        Functional.MyFunctionalInterface obj2 = () ->
+            System.out.println("This is printing from the lambda");
+        obj2.method();
+
+        Functional.AnotherFunctInter obj3 = (num) -> num/2;
+        System.out.println(obj3.half(45));
+
+        Functional.TwoParameters obj4 = (num1, num2) -> num1 - num2;
+        System.out.println(obj4.difference(2, 5));
+
+
+    }
+
+}
--- src/labs_examples/lambdas/labs/LambdasEx02.java
+++ src/labs_examples/lambdas/labs/LambdasEx02.java
@@ -1,4 +1,5 @@
 package labs_examples.lambdas.labs;
+import java.util.function.BiFunction;
 
 /**
  * Lambdas Exercise 2:
@@ -7,3 +8,35 @@ package labs_examples.lambdas.labs;
  *      10 of them! You can do it! You'll know so much about them when you're done!
  *
  */
+
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.DoubleBinaryOperator;
+
+
+public class LambdasEx02 {
+
+    public static void main(String[] args) {
+
+
+        Predicate<Integer> obj = num -> {
+            if (num % 2 == 0) {
+                return true;
+            }
+            return false;
+        };
+
+        boolean obj1 = obj.test(1981);
+        System.out.println(obj1);
+
+
+        DoubleBinaryOperator dbo = (double a, double b) -> a * b;
+        System.out.println(dbo.applyAsDouble(3.1,6.9));
+
+
+        Supplier<Double> random = () -> Math.random();
+                System.out.println(random.get());
+
+    }
+
+}
